005ad340cf424705598c47ede884d11228bfc337
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-04-21 21:01:02 +0530

    orm_Api

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,38 +1,59 @@
 const db=require('../utils/db-connection');
+const Student=require('../models/students')
+const addEntries=async (req,res)=>{
+  try{
+    const {email,name}=req.body;
+    const student = await Student.create({
+      email:email,
+      name:name
+    });
+    res.status(201).send(`User with name: ${name} is created!`)
 
-const addEntries=(req,res)=>{
-  const {email,name}=req.body;
-  const insertQuery='INSERT INTO students (email,name) VALUES(?,?,)';
-  db.execute(insertQuery,[email,name],(err)=>{
-    if(err){
-      console.log(err.message);
-      res.status(500).send(err.message);
-      db.end();
-      return ;
-    }
-    console.log("value has been inserted");
-    res.status(200).send(`Student with name ${name} successfully added`)
-  })
+  } catch(error){
+     res.status(500).send('Unable to make an entry');
+  }
+ 
 }
 
-const updateEntry=(req,res)=>{
-  const {id}=req.params;
-  const {name}=req.body;
-  const updateQuery="UPDATE Students set name=? WHERE id=?";
-  db.execute(updateQuery,[name,id],(err,result)=>{
-    if(err){
-      console.log(err.message);
-      res.status(500).send(err.message);
-      return ;
+const updateEntry=async (req,res)=>{
+  
+  try {
+    const {id}=req.params;
+    const {name}=req.body;
+
+    const student=await Student.findByPk(id);
+    if(!student){
+      res.status(404).send("User is not found");
     }
-    if(result.affectedRows===0){
-      res.status(404).send("Student not found");
-      return ;
+    student.name=name;
+    await student.save();
+    res.status(200).send("User has been updated!")
+
+  } catch (error) {
+    res.status(500).send("User cannot be updated")
+  }
+    
+}
+
+const deleteEntry=async (req,res)=>{
+  try {
+    const {id}=req.params;
+    const student=await Student.destroy({
+      where:{
+        id:id
+      }
+    })
+    if(!student){
+      res.status(404).send('User is not found');
     }
-    res.status(200).send("user has been updated");
-  })
+    res.status(200).send('User is deleted');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error encountered while deleting.')
+  }
 }
 module.exports={
  addEntries,
- updateEntry
+ updateEntry,
+ deleteEntry
 }
--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -2,10 +2,11 @@ const express=require('express');
 const studentController=require('../controller/studentController');
 const router=express.Router();
 
-router.post("/users",studentController.addEntries);
+router.post("/add",studentController.addEntries);
 
 router.put('/update/:id',studentController.updateEntry);
 
+router.delete('/delete/:id',studentController.deleteEntry);
 
 
 module.exports=router;
